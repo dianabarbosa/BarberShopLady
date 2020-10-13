dcd5a90025b6d159a459dd203807a21f6ffc6296
dianabarbosa <diana2011barbosa@gmail.com>
2020-10-13 11:56:14 -0300

    7 alteração commit

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                    <hr>
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item alert-primary active">
                             <a class="nav-link" href="index.php">Inicial<span class="sr-only">Barber shop</span></a>
