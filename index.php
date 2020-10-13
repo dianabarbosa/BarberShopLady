3600384a003bb019ef31e6f0992ad4d58aa52996
dianabarbosa <diana2011barbosa@gmail.com>
2020-10-13 11:54:22 -0300

    6 alteração commit

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
             <!--Início header-->
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="#"></a>
+                <hr>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
                     <span class="navbar-toggler-icon"></span>
                 </button>
